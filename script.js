85c321539f13cd953617597f371cb921e222928e
hnichm <nmhcoding@gmail.com>
2024-02-12 11:21:03 -0600

    Created extremely simple combat loop, DRY for AA timers next.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 class Player {
-  constructor(health, damage) {
+  constructor(health, damage, autoAttackTime) {
     this.health = health;
     this.damage = damage;
+    this.autoAttackAmount = 0;
+    this.autoAttackTime = 2000;
   }
 
   takeDamage(damage) {
@@ -10,12 +12,39 @@ class Player {
 }
 
 class Enemy {
-  constructor(health, damage) {
+  constructor(health, damage, autoAttackTime) {
     this.health = health;
     this.damage = damage;
+    this.autoAttackAmount = 0;
+    this.autoAttackTime = 3500;
   }
 
   takeDamage(damage) {
     this.health -= damage;
   }
 }
+
+let player = new Player(100, 10);
+let enemy = new Enemy(50, 3);
+
+function gameLoop() {
+  if (player.autoAttackAmount >= player.autoAttackTime) {
+    enemy.takeDamage(player.damage);
+    console.log(`Enemy took damage! Their health: ${enemy.health}`);
+    player.autoAttackAmount = 0;
+  } else {
+    player.autoAttackAmount += 100;
+  }
+
+  if (enemy.autoAttackAmount >= enemy.autoAttackTime) {
+    player.takeDamage(enemy.damage);
+    console.log(`Player took damage! Their health: ${player.health}`);
+    enemy.autoAttackAmount = 0;
+  } else {
+    enemy.autoAttackAmount += 100;
+  }
+
+  setTimeout(gameLoop, 100);
+}
+
+gameLoop();
