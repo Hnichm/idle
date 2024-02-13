50bdf1d02208ca4a3c35b6c54f8883aa6db91f1c
hnichm <nmhcoding@gmail.com>
2024-02-12 20:26:49 -0600

    DRYed attack into attack method for player and enemy

--- script.js
+++ script.js
@@ -8,24 +8,6 @@ setInterval(() => player.attack(enemy), player.autoAttackTime);
 setInterval(() => enemy.attack(player), enemy.autoAttackTime);
 
 function gameLoop() {
-  // // ! Player auto attacking
-  // if (player.autoAttackAmount >= player.autoAttackTime) {
-  //   enemy.takeDamage(player.damage);
-  //   console.log(`Enemy took damage! Their health: ${enemy.health}`);
-  //   player.autoAttackAmount = 0;
-  // } else {
-  //   player.autoAttackAmount += 100;
-  // }
-
-  // // ! Enemy auto attacking
-  // if (enemy.autoAttackAmount >= enemy.autoAttackTime) {
-  //   player.takeDamage(enemy.damage);
-  //   console.log(`Player took damage! Their health: ${player.health}`);
-  //   enemy.autoAttackAmount = 0;
-  // } else {
-  //   enemy.autoAttackAmount += 100;
-  // }
-
   // ? Player health check
   player.healthCheck();
   // ? Player regen
