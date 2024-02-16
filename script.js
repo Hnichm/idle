81b2fe229a97f0c16717e6f0cf641ad9867965e1
hnichm <nmhcoding@gmail.com>
2024-02-16 12:25:08 -0600

    Added BG image container, pulsating animation, art files

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ let enemy = new Enemy(50, 3, 2000);
 setInterval(() => player.attack(enemy), player.autoAttackTime);
 setInterval(() => enemy.attack(player), enemy.autoAttackTime);
 
-function gameLoop() {
-  // ? Player health check
-  player.healthCheck();
-  // ? Player regen
-  player.regen();
+// function gameLoop() {
+//   // ? Player health check
+//   player.healthCheck();
+//   // ? Player regen
+//   player.regen();
 
-  setTimeout(gameLoop, 100);
-}
+//   setTimeout(gameLoop, 100);
+// }
 
-gameLoop();
+// gameLoop();
